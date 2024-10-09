b78f5b7cda53dedc8d20814e218526c6732ef35b
Glas42 <145870870+Glas42@users.noreply.github.com>
2024-10-10 00:49:45 +0200

    maybe i can set the python file directly...

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="SimpleWindow",
-    version="0.4",
+    version="0.5",
     description="A package to easily create windows in Python using PyOpenGL, GLFW, OpenCV-Python, NumPy, pywin32 and ctypes",
     long_description=open("README.md").read(),
     author="Glas42",
     license="GPL-3.0",
-    packages=["SimpleWindow"],
+    packages=["SimpleWindow.py"],
     python_requires=">=3.9",
     install_requires=[
         "pyopengl",
