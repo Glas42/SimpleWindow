c758f454a5d7d37384b679c9e7fd9baa665f6786
Glas42 <145870870+Glas42@users.noreply.github.com>
2024-10-09 23:49:41 +0200

    maybe fixed it now

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     version="0.1",
     description="A package to easily create windows in Python using PyOpenGL, GLFW, OpenCV-Python, NumPy, pywin32 and ctypes",
     long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
     author="Glas42",
     license="GPL-3.0",
     packages=find_packages(),
