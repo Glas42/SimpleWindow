9f4ee803a952aa29d27c7d4b131042740e7069e0
Glas42 <145870870+Glas42@users.noreply.github.com>
2024-10-10 01:10:22 +0200

    version update to push changes on pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="SimpleWindow",
-    version="0.6",
+    version="0.7",
     description="A package to easily create windows in Python using PyOpenGL, GLFW, OpenCV-Python, NumPy, pywin32 and ctypes",
     long_description=open("README.md").read(),
     author="Glas42",
