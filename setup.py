42f7a2b4a49a5499d99c8a1e4d1e6460bcea7eb3
Glas42 <145870870+Glas42@users.noreply.github.com>
2024-10-13 07:50:19 +0200

    Fixed that i didnt update the __init__.py in the last update.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="SimpleWindow",
-    version="0.14",
+    version="0.15",
     description="A package to easily create windows in Python using GLFW, OpenCV-Python, NumPy, pywin32 and ctypes.",
     long_description=open("README.md").read(),
     author="Glas42",
